5614f04abbabc620fcdd43f480c385f0e573d4ba
Randamus <rcviets@gmail.com>
2020-03-08 13:55:33 -0600

    start lights background styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 const startGame = () => {
     document.getElementById('startButton').style.display = 'none';
     document.getElementById('startLights').style.display = 'inline-block';
-    document.getElementsByTagName('h1')[0].innerHTML = 'Get Ready!';
+    document.getElementsByTagName('h1')[0].innerHTML = 'ON YOUR MARK!... GET SET!...';
 };
