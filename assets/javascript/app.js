faffe5db4946ac3d60946bd3527adb3dc3e36e65
Randamus <rcviets@gmail.com>
2020-03-07 19:58:34 -0700

    ready to be animated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 const startGame = () => {
     document.getElementById('startButton').style.display = 'none';
     document.getElementById('startLights').style.display = 'inline-block';
+    document.getElementsByTagName('h1')[0].innerHTML = 'On Your Mark!';
 };
