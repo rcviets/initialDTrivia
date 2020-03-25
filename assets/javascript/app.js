47bb57586729657f228c7994d384133a90f62fda
Randamus <rcviets@gmail.com>
2020-03-24 18:54:12 -0600

    startLights div disappears

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,12 @@
 const startGame = () => {
+    start1();
+    setTimeout(endStart1 = () => {
+        document.getElementById('startLights').style.display = 'none';
+    }, 3500);;
+};
+
+const start1 = () => {
     document.getElementById('startButton').style.display = 'none';
     document.getElementById('startLights').style.display = 'inline-block';
-    document.getElementsByTagName('h1')[0].innerHTML = ' ';
-};
+    document.getElementsByTagName('h1')[0].innerHTML = 'On Your Mark!... Get Set!...';
+}
